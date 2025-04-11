9f6eaf6a8c45ad493c2d6523b2a0b427d34d6cdd
Olivia Ruiz <omr24100@email.vccs.edu>
2025-04-10 22:12:29 -0400

    Branch 2

--- random number guessing game.cpp
+++ random number guessing game.cpp
@@ -10,47 +10,103 @@ to high display "To high, try again" and when the number is guessed to low displ
 */
 
 #include <iostream>
-#include <cstdlib>  
-#include <ctime>    
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
 
 using namespace std;
 
-int main() {
-   
-    srand(static_cast<unsigned int>(time(0)));
+void displayMenu() {
+    cout << "\n----- NUMBER GUESSING GAME MENU -----" << endl;
+    cout << "1. Display Existing Sessions" << endl;
+    cout << "2. Play a New Session" << endl;
+    cout << "3. Delete Previous Sessions" << endl;
+    cout << "4. Exit the Program" << endl;
+    cout << "Choose an option (1-4): ";
+}
 
-   
+void displaySessions() {
+    ifstream inFile("sessions.txt");
+    if (!inFile) {
+        cout << "\nNo session history found." << endl;
+        return;
+    }
+
+    string line;
+    cout << "\n--- Previous Sessions ---" << endl;
+    while (getline(inFile, line)) {
+        cout << line << endl;
+    }
+    inFile.close();
+}
+
+void playNewSession() {
+    string playerName;
+    cout << "\nEnter your name: ";
+    cin.ignore(); // Clear newline from previous input
+    getline(cin, playerName);
+
+    srand(static_cast<unsigned int>(time(0)));
     int secretNumber = rand() % 100 + 1;
     int guess;
+    int attempts = 0;
 
-    cout << "Guess the number (between 1 and 100): ";
+    cout << "\nHi " << playerName << "! Let's play. Guess the number between 1 and 100." << endl;
 
-    
     do {
+        cout << "Enter your guess: ";
         cin >> guess;
+        attempts++;
 
         if (guess > secretNumber) {
-            cout << "Too high, try again: ";
-        }
-        else if (guess < secretNumber) {
-            cout << "Too low, try again: ";
-        }
-        else {
-            cout << "Congratulations! You guessed the number!" << endl;
+            cout << "Too high, try again!" << endl;
+        } else if (guess < secretNumber) {
+            cout << "Too low, try again!" << endl;
+        } else {
+            cout << " Well done, " << playerName << "! You guessed the number in " << attempts << " attempt(s)!" << endl;
+
+            // Log to file
+            ofstream outFile("sessions.txt", ios::app);
+            outFile << "Player: " << playerName << " | Attempts: " << attempts << endl;
+            outFile.close();
         }
 
     } while (guess != secretNumber);
+}
+
+void deleteSessions() {
+    ofstream outFile("sessions.txt", ios::trunc); // Clears the file
+    outFile.close();
+    cout << "\nAll previous sessions have been deleted." << endl;
+}
+
+int main() {
+    int choice;
+
+    do {
+        displayMenu();
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                displaySessions();
+                break;
+            case 2:
+                playNewSession();
+                break;
+            case 3:
+                deleteSessions();
+                break;
+            case 4:
+                cout << "\nExiting program. Goodbye!" << endl;
+                break;
+            default:
+                cout << "\nInvalid choice. Please choose 1-4." << endl;
+        }
+
+    } while (choice != 4);
 
     return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
